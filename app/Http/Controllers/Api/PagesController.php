8a048dc4e47155c2ef0fcf7c336f44532c3f2c9a
zulkifli_r <muh.zul.r@gmail.com>
2019-07-18 13:41:32 +0800

    endpoint pages, opd, post dan menu

---
+++ app/Http/Controllers/Api/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    public function index(Request $request)
+    {
+        dd($request);
+    }
+
+    public function read(Request $request)
+    {
+
+        dd($request);
+    }
+}
