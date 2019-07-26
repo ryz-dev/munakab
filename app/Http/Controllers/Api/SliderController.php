87d015d3a127a47e0250f52ee57c2865c00bef08
zulkifli-r <muh.zul.r@gmail.com>
2019-07-27 04:35:03 +0800

    +slider's endpoint

--- app/Http/Controllers/Api/SliderController.php
+++ app/Http/Controllers/Api/SliderController.php
@@ -7,63 +7,78 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 use League\Flysystem\Plugin\ListWith;
 use Illuminate\Support\Str;
+use App\Slider;
 
 class SliderController extends Controller
 {
-    public function index(Request $request)
+
+    public function index()
     {
-        $options = $request->details ?? [];
-
-        $thumbnail_names = [];
-        $thumbnails = [];
-        if (!($options->hide_thumbnails ?? false)) {
-            $thumbnail_names = array_column(($options['thumbnails'] ?? []), 'name');
-        }
-        $filesystem = config('voyager.storage.disk');
-
-        $folder = 'slider';
-
-        if ($folder == '/') {
-            $folder = '';
-        }
-
-        $dir = ''.$folder;
-
-        $files = [];
-        $storage = Storage::disk($filesystem)->addPlugin(new ListWith());
-        $storageItems = $storage->listWith(['mimetype'], $dir);
-
-        foreach ($storageItems as $item) {
-            if (empty(pathinfo($item['path'], PATHINFO_FILENAME)) && !config('voyager.hidden_files')) {
-                continue;
-            }
-            // Its a thumbnail and thumbnails should be hidden
-            if (Str::endsWith($item['filename'], $thumbnail_names)) {
-                $thumbnails[] = $item;
-                continue;
-            }
-            $files[] = [
-                'name'          => $item['basename'],
-                'filename'      => $item['filename'],
-                'type'          => $item['mimetype'] ?? 'file',
-                'path'          => Storage::disk($filesystem)->url($item['path']),
-                'relative_path' => $item['path'],
-                'size'          => $item['size'],
-                'last_modified' => $item['timestamp'],
-                'thumbnails'    => [],
+        $slider = new Slider();
+        $slider = $slider->all()->map(function($value){
+            return [
+                'deskripsi' => $value->description,
+                'image' => asset($value->image)
             ];
-        }
+        });
 
-        foreach ($files as $key => $file) {
-            foreach ($thumbnails as $thumbnail) {
-                if ($file['type'] != 'folder' && Str::startsWith($thumbnail['filename'], $file['filename'])) {
-                    $thumbnail['thumb_name'] = str_replace($file['filename'].'-', '', $thumbnail['filename']);
-                    $thumbnail['path'] = Storage::disk($filesystem)->url($thumbnail['path']);
-                    $files[$key]['thumbnails'][] = $thumbnail;
-                }
-            }
-        }
-
-        return apiResponse(200,$files);
+        return apiResponse(200,$slider->toArray());
     }
+
+    // public function index(Request $request)
+    // {
+    //     $options = $request->details ?? [];
+
+    //     $thumbnail_names = [];
+    //     $thumbnails = [];
+    //     if (!($options->hide_thumbnails ?? false)) {
+    //         $thumbnail_names = array_column(($options['thumbnails'] ?? []), 'name');
+    //     }
+    //     $filesystem = config('voyager.storage.disk');
+
+    //     $folder = 'slider';
+
+    //     if ($folder == '/') {
+    //         $folder = '';
+    //     }
+
+    //     $dir = ''.$folder;
+
+    //     $files = [];
+    //     $storage = Storage::disk($filesystem)->addPlugin(new ListWith());
+    //     $storageItems = $storage->listWith(['mimetype'], $dir);
+
+    //     foreach ($storageItems as $item) {
+    //         if (empty(pathinfo($item['path'], PATHINFO_FILENAME)) && !config('voyager.hidden_files')) {
+    //             continue;
+    //         }
+    //         // Its a thumbnail and thumbnails should be hidden
+    //         if (Str::endsWith($item['filename'], $thumbnail_names)) {
+    //             $thumbnails[] = $item;
+    //             continue;
+    //         }
+    //         $files[] = [
+    //             'name'          => $item['basename'],
+    //             'filename'      => $item['filename'],
+    //             'type'          => $item['mimetype'] ?? 'file',
+    //             'path'          => Storage::disk($filesystem)->url($item['path']),
+    //             'relative_path' => $item['path'],
+    //             'size'          => $item['size'],
+    //             'last_modified' => $item['timestamp'],
+    //             'thumbnails'    => [],
+    //         ];
+    //     }
+
+    //     foreach ($files as $key => $file) {
+    //         foreach ($thumbnails as $thumbnail) {
+    //             if ($file['type'] != 'folder' && Str::startsWith($thumbnail['filename'], $file['filename'])) {
+    //                 $thumbnail['thumb_name'] = str_replace($file['filename'].'-', '', $thumbnail['filename']);
+    //                 $thumbnail['path'] = Storage::disk($filesystem)->url($thumbnail['path']);
+    //                 $files[$key]['thumbnails'][] = $thumbnail;
+    //             }
+    //         }
+    //     }
+
+    //     return apiResponse(200,$files);
+    // }
 }
