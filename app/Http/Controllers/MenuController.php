7cae554381e9aeab8a762291be0f197278b99902
zulkifli-r <muh.zul.r@gmail.com>
2019-09-14 15:29:55 +0800

    menus controller untuk menu builder

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use TCG\Voyager\Facades\Voyager;
+use Illuminate\Support\Arr;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class MenuController extends VoyagerBaseController
+{
+    public function index(Request $request)
+    {
+        // dd('halo');
+        // GET THE SLUG, ex. 'posts', 'pages', etc.
+        $slug = $this->getSlug($request);
+
+        // GET THE DataType based on the slug
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('browse', app($dataType->model_name));
+
+        $getter = $dataType->server_side ? 'paginate' : 'get';
+
+        $search = (object) ['value' => $request->get('s'), 'key' => $request->get('key'), 'filter' => $request->get('filter')];
+        $searchable = $dataType->server_side ? array_keys(SchemaManager::describeTable(app($dataType->model_name)->getTable())->toArray()) : '';
+        $orderBy = $request->get('order_by', $dataType->order_column);
+        $sortOrder = $request->get('sort_order', null);
+        $usesSoftDeletes = false;
+        $showSoftDeleted = false;
+        $orderColumn = [];
+        if ($orderBy) {
+            $index = $dataType->browseRows->where('field', $orderBy)->keys()->first() + 1;
+            $orderColumn = [[$index, 'desc']];
+            if (!$sortOrder && isset($dataType->order_direction)) {
+                $sortOrder = $dataType->order_direction;
+                $orderColumn = [[$index, $dataType->order_direction]];
+            } else {
+                $orderColumn = [[$index, 'desc']];
+            }
+        }
+
+        // Next Get or Paginate the actual content from the MODEL that corresponds to the slug DataType
+        if (strlen($dataType->model_name) != 0) {
+            $model = app($dataType->model_name);
+
+            if ($dataType->scope && $dataType->scope != '' && method_exists($model, 'scope'.ucfirst($dataType->scope))) {
+                $query = $model->{$dataType->scope}();
+            } else {
+                $query = $model::select('*');
+            }
+
+            // Use withTrashed() if model uses SoftDeletes and if toggle is selected
+            if ($model && in_array(SoftDeletes::class, class_uses($model)) && app('VoyagerAuth')->user()->can('delete', app($dataType->model_name))) {
+                $usesSoftDeletes = true;
+
+                if ($request->get('showSoftDeleted')) {
+                    $showSoftDeleted = true;
+                    $query = $query->withTrashed();
+                }
+            }
+
+            // If a column has a relationship associated with it, we do not want to show that field
+            $this->removeRelationshipField($dataType, 'browse');
+
+            if ($search->value != '' && $search->key && $search->filter) {
+                $search_filter = ($search->filter == 'equals') ? '=' : 'LIKE';
+                $search_value = ($search->filter == 'equals') ? $search->value : '%'.$search->value.'%';
+                $query->where($search->key, $search_filter, $search_value);
+            }
+
+            if ($orderBy && in_array($orderBy, $dataType->fields())) {
+                $querySortOrder = (!empty($sortOrder)) ? $sortOrder : 'desc';
+                $dataTypeContent = call_user_func([
+                    $query->orderBy($orderBy, $querySortOrder),
+                    $getter,
+                ]);
+            } elseif ($model->timestamps) {
+                $dataTypeContent = call_user_func([$query->latest($model::CREATED_AT), $getter]);
+            } else {
+                $dataTypeContent = call_user_func([$query->orderBy($model->getKeyName(), 'DESC'), $getter]);
+            }
+
+            // Replace relationships' keys for labels and create READ links if a slug is provided.
+            $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType);
+        } else {
+            // If Model doesn't exist, get data from table name
+            $dataTypeContent = call_user_func([DB::table($dataType->name), $getter]);
+            $model = false;
+        }
+
+        // Check if BREAD is Translatable
+        if (($isModelTranslatable = is_bread_translatable($model))) {
+            $dataTypeContent->load('translations');
+        }
+
+        // Check if server side pagination is enabled
+        $isServerSide = isset($dataType->server_side) && $dataType->server_side;
+
+        // Check if a default search key is set
+        $defaultSearchKey = $dataType->default_search_key ?? null;
+
+        $view = 'voyager::bread.browse';
+
+        if (view()->exists("voyager::$slug.browse")) {
+            $view = "voyager::$slug.browse";
+        }
+
+        return Voyager::view($view, compact(
+            'dataType',
+            'dataTypeContent',
+            'isModelTranslatable',
+            'search',
+            'orderBy',
+            'orderColumn',
+            'sortOrder',
+            'searchable',
+            'isServerSide',
+            'defaultSearchKey',
+            'usesSoftDeletes',
+            'showSoftDeleted'
+        ));
+    }
+
+    public function builder($id)
+    {
+        // dd('$id');
+        $menu = Voyager::model('Menu')->findOrFail($id);
+
+        $this->authorize('edit', $menu);
+
+        $isModelTranslatable = is_bread_translatable(Voyager::model('MenuItem'));
+
+        return Voyager::view('voyager::menus.builder', compact('menu', 'isModelTranslatable'));
+    }
+
+    public function delete_menu($menu, $id)
+    {
+        $item = Voyager::model('MenuItem')->findOrFail($id);
+
+        $this->authorize('delete', $item);
+
+        $item->deleteAttributeTranslation('title');
+
+        $item->destroy($id);
+
+        return redirect()
+            ->route('voyager.menus.builder', [$menu])
+            ->with([
+                'message'    => __('voyager::menu_builder.successfully_deleted'),
+                'alert-type' => 'success',
+            ]);
+    }
+
+    public function add_item(Request $request)
+    {
+        $menu = Voyager::model('Menu');
+
+        $this->authorize('add', $menu);
+
+        $data = $this->prepareParameters(
+            $request->all()
+        );
+
+        unset($data['id']);
+        $data['order'] = Voyager::model('MenuItem')->highestOrderMenuItem();
+
+        // Check if is translatable
+        $_isTranslatable = is_bread_translatable(Voyager::model('MenuItem'));
+        if ($_isTranslatable) {
+            // Prepare data before saving the menu
+            $trans = $this->prepareMenuTranslations($data);
+        }
+
+        $menuItem = Voyager::model('MenuItem')->create($data);
+
+        // Save menu translations
+        if ($_isTranslatable) {
+            $menuItem->setAttributeTranslations('title', $trans, true);
+        }
+
+        return redirect()
+            ->route('voyager.menus.builder', [$data['menu_id']])
+            ->with([
+                'message'    => __('voyager::menu_builder.successfully_created'),
+                'alert-type' => 'success',
+            ]);
+    }
+
+    public function update_item(Request $request)
+    {
+        $id = $request->input('id');
+        $data = $this->prepareParameters(
+            $request->except(['id'])
+        );
+
+        $menuItem = Voyager::model('MenuItem')->findOrFail($id);
+
+        $this->authorize('edit', $menuItem->menu);
+
+        if (is_bread_translatable($menuItem)) {
+            $trans = $this->prepareMenuTranslations($data);
+
+            // Save menu translations
+            $menuItem->setAttributeTranslations('title', $trans, true);
+        }
+
+        $menuItem->update($data);
+
+        return redirect()
+            ->route('voyager.menus.builder', [$menuItem->menu_id])
+            ->with([
+                'message'    => __('voyager::menu_builder.successfully_updated'),
+                'alert-type' => 'success',
+            ]);
+    }
+
+    public function order_item(Request $request)
+    {
+        $menuItemOrder = json_decode($request->input('order'));
+
+        $this->orderMenu($menuItemOrder, null);
+    }
+
+    private function orderMenu(array $menuItems, $parentId)
+    {
+        foreach ($menuItems as $index => $menuItem) {
+            $item = Voyager::model('MenuItem')->findOrFail($menuItem->id);
+            $item->order = $index + 1;
+            $item->parent_id = $parentId;
+            $item->save();
+
+            if (isset($menuItem->children)) {
+                $this->orderMenu($menuItem->children, $item->id);
+            }
+        }
+    }
+
+    protected function prepareParameters($parameters)
+    {
+        switch (Arr::get($parameters, 'type')) {
+            case 'route':
+                $parameters['url'] = null;
+                break;
+            default:
+                $parameters['route'] = null;
+                $parameters['parameters'] = '';
+                break;
+        }
+
+        if (isset($parameters['type'])) {
+            unset($parameters['type']);
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * Prepare menu translations.
+     *
+     * @param array $data menu data
+     *
+     * @return JSON translated item
+     */
+    protected function prepareMenuTranslations(&$data)
+    {
+        $trans = json_decode($data['title_i18n'], true);
+
+        // Set field value with the default locale
+        $data['title'] = $trans[config('voyager.multilingual.default', 'en')];
+
+        unset($data['title_i18n']);     // Remove hidden input holding translations
+        unset($data['i18n_selector']);  // Remove language selector input radio
+
+        return $trans;
+    }
+}
